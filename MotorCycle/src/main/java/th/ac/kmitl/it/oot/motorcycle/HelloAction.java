6720152399539cdfc2e58863c4d4441ac0e2e5e9
Chayanon <chayanon.s@gmail.com>
2019-11-12 18:13:49 +0700

    add hello.java

---
+++ MotorCycle/src/main/java/th/ac/kmitl/it/oot/motorcycle/HelloAction.java
@@ -0,0 +1,19 @@
+package th.ac.kmitl.it.oot.motorcycle;
+
+//import org.apache.struts.helloworld.model.MessageStore;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+    private MessageStore messageStore;
+
+    public String execute() {
+        messageStore = new MessageStore() ;
+        
+        return SUCCESS;
+    }
+
+    public MessageStore getMessageStore() {
+        return messageStore;
+    }
+}
